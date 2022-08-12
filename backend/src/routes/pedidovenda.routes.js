ddf7ebeabb6086251cdf986d3d9ba31d3b6e67ff
Rhenan <diasrhenan@gmail.com>
2022-08-12 09:54:19 -0300

    Corrigino método da rota de sincronização de pedidos de venda

--- backend/src/routes/pedidovenda.routes.js
+++ backend/src/routes/pedidovenda.routes.js
@@ -2,7 +2,7 @@ const controller = require("../controllers/venda.controller");
 const { protectedRoute } = require("../middlewares/auth");
 
 function load(routes) {
-  routes.post("/pedidovenda/sincroniza", protectedRoute, controller.sincronizaPedidosVenda);
+  routes.get("/pedidovenda/sincroniza", protectedRoute, controller.sincronizaPedidosVenda);
   routes.post("/pedidovenda/callback", controller.callbackVendas);
 }
 
