83df1f8458c2a7b93c6124bd4218e193c8737598
rhenandias <diasrhenan@gmail.com>
2023-04-03 13:35:21 -0300

    Primeira versão do backend do dashboard de budget de compras

--- backend/src/business/budgetcompras.business.js
+++ backend/src/business/budgetcompras.business.js
@@ -1,10 +1,13 @@
 const { ok } = require("../modules/http");
-
 const { models } = require("../modules/sequelize");
 const { Op } = require("sequelize");
 const dayjs = require("dayjs");
 const { dictionary } = require("../utils/dict");
 const currency = require("currency.js");
+
+const dayjsBusinessDays = require("dayjs-business-days2").default;
+
+dayjs.extend(dayjsBusinessDays);
 
 module.exports = {
   // Objetivos
@@ -21,8 +24,14 @@ module.exports = {
   // Progresso Nacional
   // Progresso Internacional
 
-  async dashboard() {
-    // Adquirir as categorias dos pedidos de compra
+  async montarDicionarioCategorias() {
+    // Enumera as categorias de pedidos de compra em formato de dicionario
+
+    // {
+    //   "Nacional": "ID",
+    //   "Internacional": "ID"
+    // }
+
     const categoriasPedidoCompra = await models.tbcategoriapedidocompra.findAll({
       attributes: ["idcategoria", "descricao"],
       where: {
@@ -33,17 +42,23 @@ module.exports = {
       raw: true,
     });
 
-    // Enumera as categorias de pedido de compra em formato de dicionário
-    const dicionarioCategorias = dictionary(categoriasPedidoCompra, "descricao");
+    let dicionarioCategorias = {};
 
-    // Definir o início do mês vigente para cálculo de budget
-    // const inicioMes = dayjs().startOf("month").format("YYYY-MM-DD HH:mm:ss");
-    // const finalMes = dayjs().endOf("month").format("YYYY-MM-DD HH:mm:ss");
+    categoriasPedidoCompra.forEach((categoria) => {
+      dicionarioCategorias[categoria.descricao] = categoria.idcategoria;
+    });
 
-    const inicioMes = "2023-03-01 00:00:00";
-    const finalMes = "2023-03-31 00:00:00";
+    return dicionarioCategorias;
+  },
 
-    // Adquirir os budgets presentes para o início do mês vigente
+  async adquirirBudgets(inicioMes) {
+    // Adquire os budgets referente ao mês vigente informado e retorna em formato de dicionário
+
+    // {
+    //   "idCategoria": valorBudget,
+    //   "idCategoria": valorBudget
+    // }
+
     const budgetsFiltrados = await models.tbbudgetcompras.findAll({
       attributes: ["idbudget", "datainicio", "valor", "idcategoria"],
       where: {
@@ -52,17 +67,16 @@ module.exports = {
       raw: true,
     });
 
-    // Definir variáveis de budgets Nacional e Internacional
     let budgets = {};
 
     budgetsFiltrados.forEach((budget) => {
       budgets[budget.idcategoria] = budget.valor;
     });
 
-    let budgetNacional = budgets[dicionarioCategorias["Nacional"].idcategoria] || 0;
-    let budgetInternacional = budgets[dicionarioCategorias["Internacional"].idcategoria] || 0;
+    return budgets;
+  },
 
-    // Adquirir os pedidos de compra dentro do período do mês vigente
+  async queryPedidosCompra(inicioMes, finalMes) {
     const pedidosPeriodo = await models.tbpedidocompra.findAll({
       attributes: [
         "idpedidocompra",
@@ -80,28 +94,99 @@ module.exports = {
       include: [
         {
           model: models.tbparcelapedidocompra,
+          attributes: ["idparcela", "valor", "idformapagamento"],
           required: true,
         },
       ],
       nest: true,
-      raw: true,
     });
 
+    return pedidosPeriodo;
+  },
+
+  async dashboard() {
+    // Montar dicionário de categorias de pedidos de compra
+    const dicionarioCategorias = await this.montarDicionarioCategorias();
+
+    // Definir o início do mês vigente para cálculo de budget
+    const inicioMes = dayjs().startOf("month").format("YYYY-MM-DD HH:mm:ss");
+    const finalMes = dayjs().endOf("month").format("YYYY-MM-DD HH:mm:ss");
+
+    // Adquirir os budgets existentes para o mês vigente
+    const budgets = await this.adquirirBudgets(inicioMes);
+
+    let budgetNacional = currency(budgets[dicionarioCategorias["Nacional"]] || 0);
+    let budgetInternacional = currency(budgets[dicionarioCategorias["Internacional"]] || 0);
+
+    // Adquirir os pedidos de compra dentro do período do mês vigente
+    const pedidosPeriodo = await this.queryPedidosCompra(inicioMes, finalMes);
+
+    // Separar lista de pedidos de budget Nacional e Internacional
     const pedidosBudgetNacional = pedidosPeriodo.filter(
-      (pedido) => pedido.idcategoria === dicionarioCategorias["Nacional"].idcategoria
+      (pedido) => pedido.idcategoria == dicionarioCategorias["Nacional"]
     );
 
     const pedidosBudgetInternacional = pedidosPeriodo.filter(
-      (pedido) => pedido.idcategoria === dicionarioCategorias["Internacional"].idcategoria
+      (pedido) => pedido.idcategoria == dicionarioCategorias["Internacional"]
     );
+
+    // Acumular valor para pedidos de budget Nacional
+    let budgetUtilizadoNacional = 0;
+    let budgetUtilizadoInternacional = 0;
+
+    pedidosBudgetNacional.forEach((pedido) => {
+      pedido.tbparcelapedidocompras.forEach((parcela) => {
+        budgetUtilizadoNacional = currency(budgetUtilizadoNacional).add(currency(parcela.valor));
+      });
+    });
+
+    pedidosBudgetInternacional.forEach((pedido) => {
+      pedido.tbparcelapedidocompras.forEach((parcela) => {
+        budgetUtilizadoInternacional = currency(budgetUtilizadoInternacional).add(currency(parcela.valor));
+      });
+    });
+
+    // Calcular budget diário Nacional e Internacional
+    const diasUteis = dayjs().businessDaysInMonth().length;
+    const diasCorridos = 1 + diasUteis + dayjs().businessDiff(dayjs().endOf("month"));
+    const diasRestantes = diasUteis - diasCorridos;
+
+    const budgetDiarioInicialNacional = budgetNacional.divide(diasUteis);
+    const budgetDiarioInicialInternacional = budgetInternacional.divide(diasUteis);
+
+    let budgetDiarioNacional = budgetNacional.subtract(budgetUtilizadoNacional).divide(diasRestantes);
+    let budgetDiarioInternacional = budgetInternacional
+      .subtract(budgetUtilizadoInternacional)
+      .divide(diasRestantes);
+
+    const budgetEsperadoNacional = budgetDiarioInicialNacional.multiply(diasCorridos);
+    const budgetEsperadoInternacional = budgetDiarioInicialInternacional.multiply(diasCorridos);
+
+    const budgetNacionalPercentual = (budgetUtilizadoNacional.divide(budgetEsperadoNacional) * 100).toFixed(
+      2
+    );
+
+    const budgetInternacionalPercentual = (
+      budgetUtilizadoInternacional.divide(budgetEsperadoInternacional) * 100
+    ).toFixed(2);
 
     return ok({
       budgetNacional,
       budgetInternacional,
+      budgetUtilizadoNacional,
+      budgetUtilizadoInternacional,
+      budgetDiarioNacional,
+      budgetDiarioInternacional,
       quantidadePedidosPeriodo: pedidosPeriodo.length,
       quantidadePedidosNacional: pedidosBudgetNacional.length,
       quantidadePedidosInternacional: pedidosBudgetInternacional.length,
-      pedidosPeriodo,
+
+      diasUteis,
+      diasCorridos,
+      diasRestantes,
+
+      budgetNacionalPercentual,
+      budgetInternacionalPercentual,
     });
   },
 };
